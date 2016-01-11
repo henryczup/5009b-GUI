c3c858fa1596173efbb06371c7034737157ac5d7
Stuart Rumley <stuart@valontechnology.com>
2016-01-11 09:56:11 -0800

    Create Menu.py

---
+++ Menu.py
@@ -0,0 +1,35 @@
+# ----- Menu -----
+#   Contains a row of labels at the top
+#   Mode Selectors
+#   Frequency Spinboxes
+#   More to come
+
+import Tkinter as tk
+import tkMessageBox
+#from functools import partial
+#import sys
+
+class VMenu( tk.Menu ):
+    
+    # ----- Constructor -----
+    def __init__( self, parent ):
+        tk.Menu.__init__( self, parent )
+        self.parent = parent
+        
+        self.fileMenu = None
+        self.helpMenu = None
+        
+        self.fileMenu = tk.Menu( self, tearoff=0 )
+        self.add_cascade( label='File', menu=self.fileMenu )
+        self.fileMenu.add_command( label='Exit', command=exit )
+        
+        self.helpMenu = tk.Menu( self, tearoff=0 )
+        self.add_cascade( label='Help', menu=self.helpMenu )
+        self.helpMenu.add_command( label='About', command = helpAbout )
+
+def helpAbout():
+    text = 'Author Peter Mckone\n' \
+           'Valon Technology\n' \
+           'Sample 5009 GUI code\n' \
+           'No Copyright'
+    tkMessageBox.showinfo( 'Help About V5009CM', text )
