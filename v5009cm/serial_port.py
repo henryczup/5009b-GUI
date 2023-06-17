aae1f53392872a6132b1741c6bca5228fd577382
Leon Riesebos <28567817+lriesebos@users.noreply.github.com>
2023-06-16 23:07:30 -0400

    serial_port: improve control flow and usage of data structures

--- v5009cm/serial_port.py
+++ v5009cm/serial_port.py
@@ -3,6 +3,10 @@ import serial
 
 class VSerialPort(serial.Serial):
 
+    _BAUDRATES = (9600, 115200)
+    _EOL = b"\r"
+    _ENCODING = "ascii"
+
     def __init__(self, port):
         # Call the base constructor
         super().__init__(port, timeout=1.0)
@@ -12,14 +16,14 @@ class VSerialPort(serial.Serial):
         self.portLineCount = 0
         self.portLineIndex = 0
 
-        for baudrate in [9600, 115200]:
+        for baudrate in self._BAUDRATES:
             # Try to open port with current settings
             self.baudrate = baudrate
             self.close()  # Close port, just in case (does nothing if port is not open)
             self.open()
 
             # Check if the device responds
-            self.write(b'\r')
+            self.write(self._EOL)
             self.readAll()
 
             if self.portLines:
@@ -29,33 +33,26 @@ class VSerialPort(serial.Serial):
         else:
             raise RuntimeError("Cannot communicate with 5009")
 
-    def writeline(self, text):
-        print(text)
-        self.write(bytearray(text, encoding="ascii") + b'\r')
+    def writeline(self, line):
+        print(line)
+        self.write(bytearray(line, encoding=self._ENCODING) + self._EOL)
 
     def readAll(self):
         # Prepare the array to hold the incoming lines of text
-        self.portLines = []
+        self.portLines.clear()
         self.portLineCount = self.portLineIndex = 0
 
-        text = self.readline().decode()
-        while True:
-            if not text:
-                return
-            print(text)
-
-            self.portLines.append(text)
+        while line := self.readline().decode(encoding=self._ENCODING):
+            print(line)
+            self.portLines.append(line)
             self.portLineCount += 1
 
             # Stop reading when we get a prompt
-            if len(text) == 5:
-                if text[4] == '>':
-                    return
+            if len(line) == 5 and line[-1] == '>':
+                break
 
-            text = self.readline().decode()
-
-    # Read from the array of previously-received lines of text
     def lineGet(self):
+        """Read from the array of previously-received lines of text."""
         i = self.portLineIndex
         self.portLineIndex += 1
         if self.portLineIndex > self.portLineCount:
